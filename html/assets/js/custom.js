f1c97eca08fab702af564a4146051ee99c92a3c8
abhidev99 <136970360+abhidev99@users.noreply.github.com>
2025-08-19 16:29:20 +0530

    add

---
+++ html/assets/js/custom.js
@@ -0,0 +1,116 @@
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#ScrollToTop').fadeIn();
+        } else {
+            $('#ScrollToTop').fadeOut();
+        }
+		
+		if ($(window).width() > 991.98) {
+			 if ($(document).scrollTop() > 50) {
+				$('#Header').addClass('fixed-top');
+				$('#Header').addClass('shrink');
+			}
+			else {
+				$('#Header').removeClass('fixed-top');
+				$('#Header').removeClass('shrink');
+			}
+		}
+    });
+
+    $('#ScrollToTop').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });  
+	
+	 
+	  
+});
+
+
+   
+$(".services-slider-wrap").slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".services-slider-wrap2").slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+
+$(".testimonial-wrap").slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    
+  ],
+});
+
